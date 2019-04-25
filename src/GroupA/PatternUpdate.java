613104b87006928b6ef65c65cddbdbd3e044f019
pankaj1010 <pankajdevgade10@gmail.com>
2019-04-25 18:27:12 +0530

    Group A commit

---
+++ src/GroupA/PatternUpdate.java
@@ -0,0 +1,34 @@
+package GroupA;
+
+public class PatternUpdate {
+
+	public static void main(String[] args) {
+		int line = 7 ;
+		char c = 'a';
+		int num =1;
+		for (int i = 1; i <=line; i++) {
+			for (int j = i; j <=line-1; j++) {
+				System.out.print(" ");
+				//System.out.println(i%3);
+			}
+			for (int j = 1; j <=i; j++) {
+				if (i%3==1) {
+					System.out.print(" * ");
+					
+				}
+				else if(i%3==2){
+					System.out.print( " "+c+" ");
+					c++;
+				}
+				else {
+					System.out.print(" "+num+" ");
+					num++;
+				}
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
--- src/GroupA/pattern.java
+++ src/GroupA/pattern.java
@@ -2,17 +2,26 @@ package GroupA;
 
 public class pattern{
 	public static void main(String[] args) {
+		/**
+		 *    O/p expected       * 
+		 *                     a b c
+		 *                    1 2 3 4         
+		 */
 		int i = 3;
 		char c ='a';
 		int k =1;
+		// this For used for line 
 		for (int j = 1; j <=i; j++) {
+			// This is used for spaces 
 			for (int space = j; space <= i-1; space++) {
 				System.out.print(" ");
 			}
+			// this if is used to print first line
 			if (j==1) {
 				System.out.print(" *");
 				System.out.println();
 			}
+			//this if is used to print second line
 			if (j==2) {
 				for (int n=1; n <=i ; n++) {
 					System.out.print(c+" ");
@@ -20,7 +29,7 @@ public class pattern{
 				}
 				System.out.println();
 			}
-				
+			// this if is used to print third line
 				if (j==3) {
 					for (int n=i; n >=0 ; n--) {
 						System.out.print(k+" ");
